5772008a2d1b924644a43843f69baac32e12049b
Adrian Thomas <thomasa8992@hotmail.com>
2020-03-30 01:40:06 -0500

    Added logic to move character by player input and added logic to make character jump

---
+++ Starting With Primitives/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float movementSpeed = 10;
+
+    private Rigidbody playerRigidBody;
+    public float jumpForce = 6;
+    public bool isOnGround;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var horizontalInput = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime * horizontalInput);
+
+        if(Input.GetKeyDown(KeyCode.Space) && isOnGround) {
+            playerRigidBody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision other) {
+        if(other.gameObject.CompareTag("Ground")) {
+            isOnGround = true;
+        }
+    }
+}
